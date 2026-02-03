eb685dc11154dc358b3439cbc93eec68178e4e18
Tai Tang <taitang7711@gmail.com>
2026-02-03 09:40:04 +0700

    điều chỉnh lỗi

--- cloud.sh
+++ cloud.sh
@@ -3,4 +3,5 @@
 set -e
 
 # Add cloudflare gpg key
-docker run cloudflare/cloudflared:latest tunnel --no-autoupdate run --token eyJhIjoiZjY4Mjk2ZjI5MmQ4Y2I4YTZmNmJlY2FjMTFmZmFhMWUiLCJ0IjoiMDA2YjIyYzYtMWE3Zi00Njc2LWFkNDAtYzA5MWQxZDIxYTY1IiwicyI6IlpqYzFPVEZqTVdFdFpXUXpZaTAwTWpBMExXRmxNRFV0Tm1SallUYzRNR1pqTm1VdyJ9
+docker run -d --name cloudflared-tunnel --restart=always cloudflare/cloudflared:latest tunnel --no-autoupdate run --token eyJhIjoiZjY4Mjk2ZjI5MmQ4Y2I4YTZmNmJlY2FjMTFmZmFhMWUiLCJ0IjoiMDA2YjIyYzYtMWE3Zi00Njc2LWFkNDAtYzA5MWQxZDIxYTY1IiwicyI6IlpqYzFPVEZqTVdFdFpXUXpZaTAwTWpBMExXRmxNRFV0Tm1SallUYzRNR1pqTm1VdyJ9
+echo "CLOUDFLARE_TUNNEL_STARTED"
